d0b21999cb1231ea9cb382a1c50089e5f1657983
kris <kristiandiaz2003@gmail.com>
2024-06-18 11:11:18 -0700

    fixing cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -7,34 +7,34 @@ function Cart({cartItems,addItem, deleteItem}) {
     return (
         <div className=' flex xl:flex-row lg:flex-row md:flex-row sm:flex-row flex-col xl:my-4 lg:my-4 md:my-4 sm:my-4 '>            
             <div className=' flex flex-col xl:shadow-2xl shadow-black xl:w-4/6 xl:m-3'>
-            <h1 className='xl:text-3xl xl:px-12 xl:py-1 border-b-2 block'>Products</h1>
-            {
-            cartItems.length > 0 ? 
-            cartItems.map((i) => (
-                <div key={i.id+ "InCart"} className='flex border-b-2 items-center '>
-                    <img src={i.image} alt={i.name} className='xl:w-44 lg:w-44 md:w-44 sm:w-44 w-24 xl:h-44 lg:h-44 md:h-44 sm:h-44 h-24 prevent-select-img' />
-                    <h2 className='xl:text-xl lg:text-xl md:text-xl sm:text-xl text-md xl:w-52 lg:w-52 md:w-52 sm:w-52 w-40 xl:px-2 prevent-select'>{i.name}</h2>
-                    <div className='border-2 flex items-center rounded-lg xl:mx-5 lg:mx-5 md:mx-5 sm:mx-5 mx-2'>
-                        <FontAwesomeIcon icon={faMinus} className='prevent-select px-2 hover:text-red-700' onClick={() => {deleteItem(i)}}/>
-                        <h3 className='prevent-select xl:text-lg'>  {i.quantity} </h3>
-                        <FontAwesomeIcon icon={faPlus} className='prevent-select px-2 hover:text-blue-700' onClick={() => {addItem(i)}}/>
+                <h1 className='xl:text-3xl lg:text-3xl md:text-3xl sm:text-3xl text-2xl  xl:px-12 xl:py-1 border-b-2 block'>Products</h1>
+                {
+                cartItems.length > 0 ? 
+                cartItems.map((i) => (
+                    <div key={i.id+ "InCart"} className='flex border-b-2 items-center '>
+                        <img src={i.image} alt={i.name} className='xl:w-44 lg:w-44 md:w-44 sm:w-44 w-24 xl:h-44 lg:h-44 md:h-44 sm:h-44 h-24 prevent-select-img' />
+                        <h2 className='xl:text-xl lg:text-xl md:text-xl sm:text-xl text-md xl:w-52 lg:w-52 md:w-52 sm:w-52 w-40 xl:px-2 prevent-select'>{i.name}</h2>
+                        <div className='border-2 flex items-center rounded-lg xl:mx-5 lg:mx-5 md:mx-5 sm:mx-5 mx-2'>
+                            <FontAwesomeIcon icon={faMinus} className='prevent-select px-2 hover:text-red-700' onClick={() => {deleteItem(i)}}/>
+                            <h3 className='prevent-select xl:text-lg'>  {i.quantity} </h3>
+                            <FontAwesomeIcon icon={faPlus} className='prevent-select px-2 hover:text-blue-700' onClick={() => {addItem(i)}}/>
+                        </div>
+                        <h2 className='xl:w-16 xl:text-xl prevent-select'> 
+                        <FontAwesomeIcon icon={faDollar} className='xl:text-lg'/> 
+                                {i.price * i.quantity}</h2>
                     </div>
-                    <h2 className='xl:w-16 xl:text-xl prevent-select'> 
-                    <FontAwesomeIcon icon={faDollar} className='xl:text-lg'/> 
-                            {i.price * i.quantity}</h2>
+                )) :
+                
+                <div className='items-center h-max'>
+                    <h1 className='text-gray-400  text-xl text-center p-32'> Your cart is empty </h1>
                 </div>
-            )) :
-            
-            <div className='items-center h-max'>
-                <h1 className='text-gray-400  text-xl text-center p-32'> Your cart is empty </h1>
-            </div>
-            
-            }            
+                
+                }            
             </div>            
-            <div className='xl:px-10 lg:px-10 md:px-10 sm:px-10 px-2 fixed xl:w-1/3 lg:w-1/3 md:w-1/3 sm:w-1/3 w-full bottom-0 right-4 xl:h-128 lg:h-128 md:h-128 sm:h-128 h-40 border-2'>                        
-                <div className=' w-full xl:shadow-2xl shadow-black h-full  '>            
-                    <div className='xl:px-4 xl:py-2 border-b-2'>
-                        <h1 className='xl:text-3xl'>Resume</h1>
+            <div className='xl:px-0 lg:px-0 md:px-0 sm:px-0 px-2 fixed xl:w-1/3 lg:w-1/3 md:w-1/3 sm:w-1/3 w-full bottom-0 xl:right-4 lg:right-4 md:right-4 sm:right-4 xl:h-128 lg:h-128 md:h-128 sm:h-128 h-40 border-2 bg-white z-10'>                        
+                <div className=' w-full xl:shadow-2xl shadow-black h-full'>            
+                    <div className='xl:px-4 lg:px-4 md:px-4 sm:px-4 px-2 xl:py-2 lg:py-2 md:py-2 sm:py-2 py-1 border-b-2'>
+                        <h1 className='xl:text-3xl lg:text-3xl md:text-3xl sm:text-3xl text-2xl'>Resume</h1>
                     </div>
                     <div className='xl:m-2'>
                         <div className='xl:h-72'></div>
--- src/Components/SearchComponent.jsx
+++ src/Components/SearchComponent.jsx
@@ -37,7 +37,7 @@ function SearchComponent({searchProductsByQuery,searchItem}) {
         <>
         <header style={{'backgroundSize': "100% 100%"}} className='App-header bg-cover'>
             <div className="search-bar bg-white px-4 py-2 flex items-center">
-            <div className="gallery xl:block lg:block md:block hidden xl:w-5/6 lg:w-5/6 md:w-5/6 sm:w-5/6 h-full shadow-black shadow-inner">
+            <div className="gallery xl:block lg:block md:block hidden xl:w-2/6 lg:w-2/6 md:w-2/6 sm:w-2/6 h-full shadow-black shadow-inner">
                 <div className="img">
                     <img className='gal-img prevent-select-img' src="src/img/gallery/gallery_1.jpg" alt="" />
                 </div>
@@ -55,33 +55,32 @@ function SearchComponent({searchProductsByQuery,searchItem}) {
                 </div>
             </div>
             
-            <Link to="/" className=' xl:w-1/3 xl:h-24 xl:block xl:mx-16 lg:mx-16 md:mx-16 sm:mx-16 mx-0  '>
-                <img src="src\img\banner-venados.png" className='logo xl:block lg:block md:inline-block transition-all hidden border-red-600 prevent-select-img w-72 h-24 mx-10' alt="Venados logo" />
+            <Link to="/" className=' xl:w-80 xl:h-24 xl:block xl:mx-10 lg:mx-16 md:mx-16 sm:mx-16 mx-0  '>
+                <img src="src\img\banner-venados.png" className='logo xl:block lg:block md:inline-block transition-all hidden border-red-600 prevent-select-img w-full h-24 ' alt="Venados logo" />
             </Link>
 
-            <img src="src\img\venados-logo.png" className='xl:hidden lg:hidden md:hidden sm:hidden  border-red-600 prevent-select-img w-20  h-20 mx-4' alt="Venados logo mobile" />
+            <img src="src\img\venados-logo.png" className='xl:hidden lg:hidden md:hidden sm:hidden  border-red-600 prevent-select-img w-20 h-20 mx-4' alt="Venados logo mobile" />
 
-
-                <div className='w-2/3'>
-                <div className='border-2 xl:flex lg:flex md:flex sm:flex flex items-center '>
-                    <input 
-                    type="text"
-                    placeholder='Search your products'    
-                    className='search-input rounded-lg  p-1 h-14 xl:text-lg lg:text-lg md:text-lg sm:text-lg text-md xl:px-4 lg:px-4 md:px-2 sm:px-4 px-2 transition-all xl:w-11/12 w-10/12'     
-                    value={searchItem}
-                    onChange={searchProductsByQuery}
-                />                
-                    <div className='h-14 flex items-center border-l-2 w-20 hover:bg-red-600 m-glass-div transition-colors'>
-                        <Link to="/" className='w-full'>
-                        <FontAwesomeIcon className='w-full float-right xl:h-5 lg:h-5 md:h-5 sm:h-5 h-5 text-gray-400 m-glass transition-colors' icon={faMagnifyingGlass} />
-                        </Link>
-                    </div>                    
-                </div>
+                <div className='xl:w-3/6 lg:w-full md:w-full sm:w-full xl:mx-4 lg:mx-4 md:mx-4 sm:mx-4'>
+                    <div className='border-2 xl:flex lg:flex md:flex sm:flex flex items-center '>
+                        <input 
+                        type="text"
+                        placeholder='Search your products'    
+                        className='search-input rounded-lg  p-1 h-14 xl:text-lg lg:text-lg md:text-lg sm:text-lg text-md xl:px-4 lg:px-4 md:px-2 sm:px-4 px-2 transition-all xl:w-11/12 lg:w-11/12 md:w-11/12 sm:w-11/12 w-10/12'     
+                        value={searchItem}
+                        onChange={searchProductsByQuery}
+                    />                
+                        <div className='h-14 flex items-center border-l-2 w-20 hover:bg-red-600 m-glass-div transition-colors'>
+                            <Link to="/" className='w-full'>
+                            <FontAwesomeIcon className='w-full float-right xl:h-5 lg:h-5 md:h-5 sm:h-5 h-5 text-gray-400 m-glass transition-colors' icon={faMagnifyingGlass} />
+                            </Link>
+                        </div>                    
+                    </div>
                 </div>
                 <div className='xl:w-72 lg:w-72 md:w-72 sm:w-72 xl:m-4 lg:m-4 md:m-4 sm:m-4 xl:overflow-visible lg:overflow-auto md:overflow-auto sm:overflow-auto xl:block lg:block md:block sm:block xl:px-4 lg:px-4 md:px-4 sm:px-4 items-center '>
                     <FontAwesomeIcon icon={faBars} className="text-red-600 h-10 px-4 w-5 hover:scale-125 transition-transform xl:hidden lg:hidden md:hidden sm:hidden block" onClick={()=>{deployHamburgerMenu()}}/>
                     <div>
-                    <ul id='hamburger-menu' className='xl:relative lg:relative md:relative sm:relative absolute flex'>
+                    <ul id='hamburger-menu' className='xl:relative lg:relative md:relative sm:relative absolute xl:right-10 -right-1 w-16 xl:border-0 lg:border-0 md:border-0 sm:border-0 border-2 xl:bg-transparent lg:bg-transparent md:bg-transparent sm:bg-transparent bg-white xl:flex lg:flex md:flex sm:flex z-20'>
                         <li>
                         <Link to="/cart"> 
                             <FontAwesomeIcon icon={faCartShopping} className='text-red-600 h-6 p-4  hover:scale-125 hover:text-white transition-transform xl:block lg:block md:block sm:block' /> 
